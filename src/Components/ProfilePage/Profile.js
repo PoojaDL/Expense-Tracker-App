a956836d7c7b8e75b5e2e0b664eb5c9820b88c27
PoojaDL <poojadl2002@gmail.com>
2023-08-27 22:25:21 +0530

    Edit User Details

--- src/Components/ProfilePage/Profile.js
+++ src/Components/ProfilePage/Profile.js
@@ -1,8 +1,9 @@
-import { useContext, useRef } from "react";
+import { Fragment, useContext, useRef, useState } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import AuthContext from "../../Store/auth-context";
 
 const Profile = () => {
+  const [userData, setData] = useState(null);
   const nameInput = useRef();
   const imageInput = useRef();
   const authCtx = useContext(AuthContext);
@@ -45,31 +46,72 @@ const Profile = () => {
       .catch((error) => console.log(error));
   };
 
-  return (
-    <Form onSubmit={formSubmitHandler} bg="black">
-      <h1>Contact details</h1>
-      <Form.Group
-        className="mb-3"
-        controlId="exampleForm.ControlInput1"
-        aria-required
-      >
-        <Form.Label>Full Name</Form.Label>
-        <Form.Control ref={nameInput} type="text" />
-      </Form.Group>
-      <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
-      <InputGroup className="mb-3">
-        <InputGroup.Text id="basic-addon3">
-          https://example.com/users/
-        </InputGroup.Text>
-        <Form.Control
-          ref={imageInput}
-          id="basic-url"
-          aria-describedby="basic-addon3"
-        />
-      </InputGroup>
+  const fetchData = () => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyC8wHsJTe8rHFeXPPHA5u0R9NWkWsuix3s",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: data.idToken,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = data.error.message;
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        setData(data.users[0]);
+      })
+      .catch((error) => console.log(error));
+  };
 
-      <Button type="submit">Submit</Button>
-    </Form>
+  return (
+    <Fragment>
+      <Form onSubmit={formSubmitHandler} bg="black">
+        <h1>Contact details</h1>
+        <Form.Group
+          className="mb-3"
+          controlId="exampleForm.ControlInput1"
+          aria-required
+        >
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control ref={nameInput} type="text" />
+        </Form.Group>
+        <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon3">
+            https://example.com/users/
+          </InputGroup.Text>
+          <Form.Control
+            ref={imageInput}
+            id="basic-url"
+            aria-describedby="basic-addon3"
+          />
+        </InputGroup>
+
+        <Button type="submit">Submit</Button>
+      </Form>
+
+      <h1>User Details</h1>
+      <Button onClick={fetchData}>fetch user details</Button>
+      {userData && (
+        <ul>
+          <li>{`Name: ${userData.displayName}`}</li>
+          <li>{`email: ${userData.email}`}</li>
+          <li>{`image: ${userData.photoUrl}`}</li>
+        </ul>
+      )}
+    </Fragment>
   );
 };
 
