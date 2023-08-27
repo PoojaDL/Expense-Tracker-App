14054e21cee3be39bd7fab51d3eaaa7a44ea73ee
PoojaDL <poojadl2002@gmail.com>
2023-08-27 15:50:31 +0530

    Added login and SignUp pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Fragment } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Login from "./Components/Login/Login";
+import Home from "./Components/HomePage/Home";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <switch>
+        <Route path="*">
+          <Redirect to="/Login" />
+        </Route>
+        <Route path="/Login" exact>
+          <Login />
+        </Route>
+        <Route path="/Home">
+          <Home />
+        </Route>
+      </switch>
+    </main>
   );
 }
 
---
+++ src/Components/HomePage/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1>Hwllo This is home page</h1>;
+};
+
+export default Home;
