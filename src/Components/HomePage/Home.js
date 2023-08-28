3f55a55c3a4414e4e3077ce4481d25a528c613f2
PoojaDL <poojadl2002@gmail.com>
2023-08-28 12:02:22 +0530

    Added varification for user email

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,9 +1,42 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../../Store/auth-context";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Button } from "react-bootstrap";
 
 const Home = () => {
+  const [content, setContent] = useState("");
   const authCtx = useContext(AuthContext);
+
+  let data = authCtx.token;
+  if (typeof authCtx.token === "string") {
+    data = JSON.parse(authCtx.token);
+  }
+
+  const verifyEmail = (event) => {
+    event.preventDefault();
+    console.log(data.idToken);
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyC8wHsJTe8rHFeXPPHA5u0R9NWkWsuix3s",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: data.idToken,
+          requestType: "VERIFY_EMAIL",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      if (res.ok) {
+        console.log(res);
+        setContent("Verification code sent successfully");
+      } else {
+        res.json().then((res) => setContent(res.error.status));
+      }
+    });
+  };
 
   return (
     <div>
@@ -12,6 +45,8 @@ const Home = () => {
         Your Profile is inComplete
         <Link to="/Profile">Complete Now</Link>
       </p>
+      <Button onClick={verifyEmail}>Verify Email</Button>
+      <p>{content}</p>
     </div>
   );
 };
