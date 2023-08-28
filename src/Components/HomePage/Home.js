ceaf4b99100d6b243e9e0ef41176a17bb2632391
PoojaDL <poojadl2002@gmail.com>
2023-08-28 18:36:45 +0530

    Added expenses form

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,11 +1,16 @@
-import { useContext, useState } from "react";
+import { Fragment, useContext, useRef, useState } from "react";
 import AuthContext from "../../Store/auth-context";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 const Home = () => {
   const [content, setContent] = useState("");
+  const [expenseList, setExpenses] = useState([]);
   const authCtx = useContext(AuthContext);
+
+  const expenseInput = useRef();
+  const descInput = useRef();
+  const categoryInput = useRef();
 
   let data = authCtx.token;
   if (typeof authCtx.token === "string") {
@@ -40,22 +45,74 @@ const Home = () => {
     authCtx.logout();
   };
 
+  const expenseFormSubmit = (event) => {
+    event.preventDefault();
+
+    setExpenses((prevExpenses) => {
+      return [
+        ...prevExpenses,
+        {
+          expense: expenseInput.current.value,
+          description: descInput.current.value,
+          category: categoryInput.current.value,
+        },
+      ];
+    });
+
+    console.log(expenseList);
+  };
+
   return (
-    <div>
-      <h1 className="d-inline">Welcome to Expense Tracker</h1>
-      <Button
-        style={{ position: "absolute", right: "10px" }}
-        onClick={logoutHandler}
-      >
-        Logout
-      </Button>
-      <p>
-        Your Profile is inComplete
-        <Link to="/Profile">Complete Now</Link>
-      </p>
-      <Button onClick={verifyEmail}>Verify Email</Button>
-      <p>{content}</p>
-    </div>
+    <Fragment>
+      <div>
+        <h1 className="d-inline">Welcome to Expense Tracker</h1>
+        <Button
+          style={{ position: "absolute", right: "10px" }}
+          onClick={logoutHandler}
+        >
+          Logout
+        </Button>
+        <p>
+          Your Profile is inComplete
+          <Link to="/Profile">Complete Now</Link>
+        </p>
+        <Button onClick={verifyEmail}>Verify Email</Button>
+        <p>{content}</p>
+      </div>
+
+      <div>
+        <Form onSubmit={expenseFormSubmit}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Expense</Form.Label>
+            <Form.Control type="number" ref={expenseInput} />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" ref={descInput} />
+          </Form.Group>
+          <Form.Label>Category</Form.Label>
+          <Form.Select aria-label="Default select example" ref={categoryInput}>
+            <option value="Petrol">Petrol</option>
+            <option value="Food">Food</option>
+            <option value="Rent">Rent</option>
+          </Form.Select>
+
+          <Button type="submit">Add Expense</Button>
+        </Form>
+      </div>
+
+      <div>
+        {expenseList.length > 0 && (
+          <ul>
+            {expenseList.map((item) => (
+              <li>
+                {`${item.expense} - ${item.description} - ${item.category}`}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
